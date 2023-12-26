68f865de4128700719ca5d14bb179578ffd3c5f0
Iskelo <iskelo.kap@gmail.com>
2023-12-26 18:40:10 +0100

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
 
-console.log(galleryItems);
+const containerImgs = document.querySelector('.gallery');
+
+const marcup = galleryItems.map(({description: desc, preview: prev, original: org}) => `
+<li class="gallery__item">
+   <a class="gallery__link" href="${org}">
+      <img class="gallery__image" src="${prev}" alt="${desc}" />
+   </a>
+</li>
+`);
+
+containerImgs.insertAdjacentHTML('beforeend', marcup.join(''));
+
+const options = {
+	captionsData: 'alt',
+	captionDelay: 250,
+   	captionPosition: 'bottom'
+};
+
+const lightbox = new SimpleLightbox('.gallery a', options);
