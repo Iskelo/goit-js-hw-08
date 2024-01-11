e797efa9d09a19ebf2d8763a8be8c976a0608bc9
Iskelo <iskelo.kap@gmail.com>
2024-01-11 08:55:36 +0100

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,30 +1,21 @@
 import Player from '@vimeo/player';
 
+import throttle from 'lodash.throttle';
+
 const iframe = document.querySelector('iframe');
-const player = new Player('vimeo-player', {
+const storageKey = 'videoplayer-current-time';
+
+
+const player = new Player(iframe, {
     id: 19231868,
     width: 640
 });
 
-player.on('play', function() {
-    console.log('played the video!');
-});
+const getCurrentTime = function (currentTime) {
+	const seconds = currentTime.seconds;
+	localStorage.setItem(storageKey, JSON.stringify(seconds));
+ };
 
-const options = {
-	width: 640,
-	loop: true
-};
+player.on('timeupdate', throttle(getCurrentTime, 1000));
 
-player.setCurrentTime(300).then(function(seconds) {
-	// seconds = the actual time that the player seeked to
-}).catch(function(error) {
-	switch (error.name) {
-		 case 'RangeError':
-			  // the time was less than 0 or greater than the video’s duration
-			  break;
-
-		 default:
-			  // some other error occurred
-			  break;
-	}
-});
+player.setCurrentTime(JSON.parse(localStorage.getItem(storageKey)) || 0);
