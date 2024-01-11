65212fa2debfc04533a465c4743c80e3e58a72cc
Iskelo <iskelo.kap@gmail.com>
2024-01-11 10:47:14 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import _throttle from 'lodash.throttle';
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+
+let dataForm={}
+
+formLoad()
+
+form.addEventListener('input', _throttle(saveForm, 500));
+
+form.addEventListener('submit', onSubmit);
+
+function saveForm(evt){
+	dataForm = JSON.parse(localStorage.getItem(STORAGE_KEY))|| {};
+	dataForm[evt.target.name] = evt.target.value;
+	localStorage.setItem(STORAGE_KEY, JSON.stringify(dataForm))
+}
+
+function formLoad() {
+	let loadForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
+	if (!loadForm) {
+		return;
+	}
+
+	dataForm = loadForm;
+	form.email.value = dataForm.email || '';
+	form.message.value = dataForm.message || '';
+}
+
+
+function onSubmit(event) {
+	event.preventDefault()
+
+	if (!event.target.email.value || !event.target.message.value) {
+		alert('Enter data');
+		return;
+	}
+
+	event.target.reset();
+	console.log(dataForm);
+	localStorage.removeItem(STORAGE_KEY);
+}
 
