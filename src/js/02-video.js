34e606f2ebc034728ec822c440604bafcb025e03
Iskelo <iskelo.kap@gmail.com>
2024-01-11 07:56:39 +0100

    INSTALL

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player('vimeo-player', {
+    id: 19231868,
+    width: 640
+});
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+const options = {
+	width: 640,
+	loop: true
+};
+
+player.setCurrentTime(300).then(function(seconds) {
+	// seconds = the actual time that the player seeked to
+}).catch(function(error) {
+	switch (error.name) {
+		 case 'RangeError':
+			  // the time was less than 0 or greater than the video’s duration
+			  break;
+
+		 default:
+			  // some other error occurred
+			  break;
+	}
+});
